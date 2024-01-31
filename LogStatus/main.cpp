8c5e7ec92daaa1bca2a6723b7fcc84d39884b1c2
charles <128108973+OldFoolAgain@users.noreply.github.com>
2024-01-31 09:41:53 -0500

    Initial Commit

---
+++ LogStatus/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LogStatus
+//
+//  Created by Charles Kerr on 1/31/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
