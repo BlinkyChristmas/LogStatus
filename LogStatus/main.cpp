7453676bce4b7672554923463957a47d3cfbb78b
skipw20 <skipw20@verizon.net>
2024-03-18 13:45:44 -0500

    Update main.cpp

--- LogStatus/main.cpp
+++ LogStatus/main.cpp
@@ -24,14 +24,14 @@ const std::string serverShow = "#99FF99"s;
 const std::string clientError = "#FF8888"s;
 const std::string neverConnected = "#FFFF99"s;
 
-std::string serverColor = neverConnected ;
+std::string serverColor = neverConnected;
 
 enum ConnectState {
     NEVER, CONNECTED, DISCONNECTED
 };
 
-auto printTable(std::ostream& output, const ServerStatusEntry &serverstatus, ClientCollection& clients, ConnectionStatus& status, ErrorCollection& clienterrors) -> void;
-auto printTableRow(std::ostream& output, const std::string& client, ConnectState state, bool audioerr, bool playerr, const std::string &playname ) -> void;
+auto printTable(std::ostream& output, const ServerStatusEntry& serverstatus, ClientCollection& clients, ConnectionStatus& status, ErrorCollection& clienterrors) -> void;
+auto printTableRow(std::ostream& output, const std::string& client, ConnectState state, bool audioerr, bool playerr, const std::string& playname, const std::string& ipAddress) -> void;
 //auto printTableRow(std::ostream& output, const std::string& client, ConnectState state, bool has_error) -> void;
 auto writeBeginning(std::ostream& output) -> void;
 auto writeEnding(std::ostream& output) -> void;
@@ -43,12 +43,12 @@ int main(int argc, const char* argv[]) {
             throw std::runtime_error("Invalid # arguments, requires: clientpath  serverlog connectpath errorpath outputpath [-update]");
         }
         auto clientpath = std::filesystem::path(argv[1]);
-        ServerStatus serverStatus ;
-        auto serverlog = std::string(argv[2]) ;
+        ServerStatus serverStatus;
+        auto serverlog = std::string(argv[2]);
         auto connectpath = std::filesystem::path(argv[3]);
         auto errorpath = std::filesystem::path(argv[4]);
         auto outputpath = std::filesystem::path(argv[5]);
-        
+
         serverStatus.load(serverlog);
         auto clientCollection = ClientCollection(clientpath);
         auto connectionStatus = ConnectionStatus(connectpath);
@@ -82,7 +82,7 @@ int main(int argc, const char* argv[]) {
             }
             writeBeginning(output);
 
-            printTable(output, serverStatus.status(),clientCollection, connectionStatus, errorCollection);
+            printTable(output, serverStatus.status(), clientCollection, connectionStatus, errorCollection);
             writeEnding(output);
             output.close();
             if (loop) {
@@ -109,14 +109,14 @@ int main(int argc, const char* argv[]) {
 auto writeBeginning(std::ostream& output) -> void {
     // start writing the document
     output << "<!DOCTYPE html>\n";
-    output << "<html>\n"; 
+    output << "<html>\n";
     output << "<head>\n";
     output << "     <meta http-equiv = \"refresh\" content = \"10\" >\n";
     output << "<style>\n";
     output << "table,th,td{border:2px solid black;border-collapse:collapse}\n";
     output << "</style>\n";
     output << "</head>\n";
- 
+
 }
 // ========================================================================
 auto writeEnding(std::ostream& output) -> void {
@@ -125,8 +125,8 @@ auto writeEnding(std::ostream& output) -> void {
 
 }
 // ===============================================================================================================================
-auto printTable(std::ostream& output, const ServerStatusEntry &serverstatus, ClientCollection& clients, ConnectionStatus& status, ErrorCollection& clienterrors) -> void {
-    
+auto printTable(std::ostream& output, const ServerStatusEntry& serverstatus, ClientCollection& clients, ConnectionStatus& status, ErrorCollection& clienterrors) -> void {
+
     serverColor = neverConnected;
     auto text = "Not Running Server"s;
     if (serverstatus.isRunning) {
@@ -141,19 +141,19 @@ auto printTable(std::ostream& output, const ServerStatusEntry &serverstatus, Cli
         serverColor = serverShow;
         text = "Playing Show"s;
     }
-
+    auto ipAddress = "No IP"s;
 
     output << "\t<table style=\"font-family: Arial;font-size:16px;margin-left:40px;\">\n";
-    output << "\t\t<thead>\n"; 
+    output << "\t\t<thead>\n";
     output << "\t\t\t<tr>\n";
-    output << "\t\t\t<th style = \"text-align:center;color:"<< serverColor <<";background-color:#303030;width:423px\" scope=\"col\">" << util::sysTimeToString(util::ourclock::now()) << "</th>\n";
+    output << "\t\t\t<th style = \"text-align:center;color:" << serverColor << ";background-color:#303030;width:423px\" scope=\"col\">" << util::sysTimeToString(util::ourclock::now()) << "</th>\n";
     output << "\t\t\t</tr>\n";
     output << "\t\t\t<tr>\n";
     output << "\t\t\t<th style = \"text-align:center;color:#FFFFFF;background-color:#303030;width:423px\" scope=\"col\">Server Status</th>\n";
     output << "\t\t\t</tr>\n";
     output << "\t\t\t<tr>\n";
     output << "\t<table style=\"font-family: Arial;font-size:14px;margin-left:40px;\">\n";
-    output << "\t\t\t<td style = \"text-align:center;color:#000000;background-color:"<< serverColor <<";width:423px\" scope=\"col\">" << text << "</td>\n";
+    output << "\t\t\t<td style = \"text-align:center;color:#000000;background-color:" << serverColor << ";width:423px\" scope=\"col\">" << text << "</td>\n";
     output << "\t\t\t</tr>\n";
     output << "\t</table>\n";
 
@@ -170,7 +170,7 @@ auto printTable(std::ostream& output, const ServerStatusEntry &serverstatus, Cli
     output << "\t\t\t<th style=\"text-align:center;background-color:#A0A0A0;width:190px\" scope=\"col\">Client/Status</th>\n";
     output << "\t\t\t<th style=\"text-align:center;background-color:#A0A0A0;width:185px\" scope=\"col\">Timestamp</th>\n";
     output << "\t\t\t<th style=\"text-align:center;background-color:#A0A0A0;width:40px\" scope=\"col\">Error</th>\n";
-     output << "\t\t\t</tr>\n";
+    output << "\t\t\t</tr>\n";
     output << "\t\t</thead>\n";
     output << "\t\t<tbody>\n";
     const auto formattime = "%a - %b %d %H:%M:%S"s;
@@ -183,29 +183,31 @@ auto printTable(std::ostream& output, const ServerStatusEntry &serverstatus, Cli
             if (status[client.name].last().state) {
                 state = ConnectState::CONNECTED;
             }
-           playname = util::sysTimeToString(status[client.name].last().timestamp,formattime);
+            playname = util::sysTimeToString(status[client.name].last().timestamp, formattime);
+            ipAddress = status[client.name].last().address;
+
         }
-       
-  //      auto has_error = false;
-  //      if (clienterrors.contains(client.name)) {
-  //          has_error = true;
-  //      }
+
+        //      auto has_error = false;
+        //      if (clienterrors.contains(client.name)) {
+        //          has_error = true;
+        //      }
         auto play_error = false;
         auto audio_error = false;
-       
+
 
         if (clienterrors.contains(client.name)) {
 
             auto eiter = clienterrors[client.name].rbegin();
             audio_error = eiter->audio_error;
             play_error = !eiter->audio_error;
-         }
+        }
 
-       
+
 
         // Now we print the entry ;
  //       printTableRow(output, client.name, state, has_error);
-        printTableRow(output, client.name, state, audio_error, play_error, playname);
+        printTableRow(output, client.name, state, audio_error, play_error, playname, ipAddress);
     }
 
 
@@ -217,8 +219,8 @@ auto printTable(std::ostream& output, const ServerStatusEntry &serverstatus, Cli
 
 // ==============================================================================================================
 //auto printTableRow(std::ostream& output, const std::string& client, ConnectState state, bool has_error) -> void {
-auto printTableRow(std::ostream& output, const std::string& client, ConnectState state, bool audioerr, bool playerr , const std::string &playname) -> void {
-    auto concolor  = neverConnected;
+auto printTableRow(std::ostream& output, const std::string& client, ConnectState state, bool audioerr, bool playerr, const std::string& playname, const std::string& ipAddress) -> void {
+    auto concolor = neverConnected;
     auto value = "Never"s;
     switch (state) {
     case ConnectState::NEVER:
@@ -242,8 +244,9 @@ auto printTableRow(std::ostream& output, const std::string& client, ConnectState
     output << "\t\t\t\t<td style=\"background-color:" << concolor << ";\">" << client << "</td>\n";
     output << "\t\t\t\t<td style=\"background-color:" << concolor << ";\">" << playname << "</td>\n";
     output << "\t\t\t\t<td " << ((audioerr || playerr) ? ("style=\"background-color:"s + clientError + ";\">") : ("style=\"background-color:"s + disconnectedColor + ";\">"s)) << ((audioerr || playerr) ? "Error" : "") << "</td>\n";
-//    output << "\t\t\t\t<td " << (playerr ? "style=\"background-color:#FF8888;\">" : "style=\"background-color:#D0D0D0;\">") << (playerr ? "Error" : "") << "</td>\n";
+    output << "\t\t\t\t<td style=\"background-color:" << concolor << ";\">" << ipAddress << "</td>\n";
+    //    output << "\t\t\t\t<td " << (playerr ? "style=\"background-color:#FF8888;\">" : "style=\"background-color:#D0D0D0;\">") << (playerr ? "Error" : "") << "</td>\n";
 //    output << "\t\t\t\t<td " << (playerr ? "style=\"background-color:#FF8888;\">" : "style=\"background-color:#D0D0D0;\">") << (playerr ? playname : "") << "</td>\n";
- 
+
     output << "\t\t\t</tr>\n";
 }
